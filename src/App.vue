e94d77b7173680abbb34d9554b89dc24a005913b
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-05-27 04:35:36 +0900

    feat : App.vue 전체 스타일 수정

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,21 @@
 
 <script setup></script>
 
-<style scoped></style>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Pretendard", "Noto Sans KR", sans-serif;
+}
+</style>
