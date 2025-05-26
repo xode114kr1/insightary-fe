ba76291c43a6e3971c2eeff287be567a132da66f
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-05-27 02:25:18 +0900

    feat : 디렉토리 구조 생성

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>홈 페이지</div>
-</template>
-
-<script setup>
-// 비워도 OK
-</script>
-
-<style scoped></style>
---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-container"></div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.home-container {
+  min-height: 100vh;
+  background-image: url("@/assets/background.png");
+  padding: 2rem;
+  font-family: "Georgia", serif;
+}
+
+.center-image {
+  width: 280px;
+  height: 400px;
+  margin: 2rem auto;
+  background-size: cover;
+  background-position: center;
+  border: 4px solid #e1d6b8;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+}
+</style>
